4482ab2659aa9367915ec4096260df35865564d5
Song-YunMin <thd930308@naver.com>
2021-02-11 00:10:55 +0900

    [ADD] create skeleton source code

---
+++ skeleton/server.js
@@ -0,0 +1,14 @@
+const express = require('express'),
+	path = require('path'),
+	app = express();
+
+app.use(express.static('client'));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+const server = app.listen(8080, () => {
+	console.log('Server started!');
+});
