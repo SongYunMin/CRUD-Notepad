493f6b2971c76ae2d0f4c2909ea116161417209a
Song-YunMin <thd930308@naver.com>
2021-02-12 18:12:14 +0900

    [UPDATE] Class Test

--- skeleton/client/notepad.js
+++ skeleton/client/notepad.js
@@ -2,11 +2,11 @@ class Notepad {
     #dom
     constructor(dom) {
         this.#dom = dom;
+        this.prepareDom();
     }
 
     prepareDom(){
+        const teml = document.querySelector('.notepad');
 
     }
-
-
 }
